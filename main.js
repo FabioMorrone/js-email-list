b8b1e1da2adbe3cb8564701639321a41eec73496
FabioMorrone <morronefabio04@gmail.com>
2025-02-27 16:14:09 +0100

    add

--- main.js
+++ main.js
@@ -9,5 +9,10 @@ const listaMailEl = document.querySelector('Mail')
 
 for (let i = 0; i < 10; i++) {
     
-    
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.response);    
+    })
+    .catch()    
 }
